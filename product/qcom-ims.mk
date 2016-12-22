f2681fcc1c3dcda2c0a34d1e90317f83e8b314d8
NeoDarkness <aldi.45ckr@gmail.com>
2016-12-22 03:39:41 -0800

    wt88047: Clean about IMS
    Change-Id: I15333bd9478d33017719d53c0c2f856fdae539e5

--- product/qcom-ims.mk
+++ product/qcom-ims.mk
@@ -1,15 +1,4 @@
 # IMS
-PRODUCT_PACKAGES += \
-    ims \
-    imscmlibrary \
-    imssettings \
-    init.qti.ims.sh
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/ims/imscm.xml:system/etc/permissions/imscm.xml \
-    $(LOCAL_PATH)/configs/ims/ims.xml:system/etc/permissions/ims.xml \
-    $(LOCAL_PATH)/configs/ims/qti_permissions.xml:system/etc/permissions/qti_permissions.xml
-
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.jbims=1 \
     persist.radio.ROTATION_ENABLE=1 \
@@ -17,6 +6,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.VT_HYBRID_ENABLE=1 \
     persist.radio.VT_USE_MDM_TIME=0
 
-# Telephony-ext
-PRODUCT_PACKAGES += telephony-ext ims-ext-common
-PRODUCT_BOOT_JARS += telephony-ext
